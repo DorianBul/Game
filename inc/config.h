00fa64427fd57192dd6a6af3627fd279209ecdbc
DorianBul <dorian.bul@gmail.com>
2019-05-11 18:31:47 +0200

    update to sync with Mariusz

--- inc/config.h
+++ inc/config.h
@@ -14,7 +14,43 @@
 
 using namespace std;
 
+enum class Color
+{
+	black, white, orange, gray, pink, yellow, purple
+};
+
 struct Config
 {
-	//static constexpr char* =
+	// const variables
+	static constexpr uint16_t menuHorizontal = 285;
+	static constexpr uint16_t menuPos(uint16_t x){ return x* 80; }
+	static constexpr uint16_t creditsPos(uint16_t x){ return x* 50; }
+
+
+	// STRINGS
+	static constexpr char tab[] = "asd";
+
+	// COLORS
+	static const ALLEGRO_COLOR getRGB(Color color)
+	{
+		if(color == Color::black) { return al_map_rgb(0,0,0); }
+		else if(color == Color::white) { return al_map_rgb(255,255,255); }
+		else if(color == Color::orange) { return al_map_rgb(255, 128, 0); }
+		else if(color == Color::gray) { return al_map_rgb(128, 128, 128); }
+		else if(color == Color::pink) { return al_map_rgb(255, 0, 127); }
+		else if(color == Color::yellow) { return al_map_rgb(255, 255, 0); }
+		else if(color == Color::purple) { return al_map_rgb(128, 64, 255); }
+		else { return al_map_rgb(0,0,0); }
+	}
+
+
+
+};
+
+struct Player
+{
+	bool moveLEFT = false;
+	bool moveRIGHT = false;
+	bool moveJUMP = false;
+	bool moveCROUCH = false;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@
 int main()
 {
 	// INITIAL PART
-	if(!al_init()) 				{ return -1;}
-	if(!al_install_keyboard()) 	{ return -1;}
-	if(!al_init_image_addon()) 	{ return -1;}
-	if(!al_init_font_addon()) 	{ return -1;}
-	if(!al_init_ttf_addon()) 	{ return -1;}
+	if(!al_init()) 				{ return -1; }
+	if(!al_install_keyboard()) 	{ return -1; }
+	if(!al_init_image_addon()) 	{ return -1; }
+	if(!al_init_font_addon()) 	{ return -1; }
+	if(!al_init_ttf_addon()) 	{ return -1; }
 
 	ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);			// 30 FPS
 	ALLEGRO_TIMER* timerMenu = al_create_timer(1.0 / 10.0);		// 10 PFS
@@ -17,25 +17,32 @@ int main()
 	ALLEGRO_DISPLAY* disp = al_create_display(800, 600);
 	ALLEGRO_FONT *fontMenu = al_load_font("AmaticSC-Regular.ttf", 80, 0);
 	ALLEGRO_FONT *font2 = al_load_font("AmaticSC-Regular.ttf", 30, 0);
-	if(!timer || !timerMenu || !queue || !disp || !fontMenu || !font2) { return -1;}
+	if(!timer || !timerMenu || !queue || !disp || !fontMenu || !font2) { return -1; }
 	al_register_event_source(queue, al_get_keyboard_event_source());
 	al_register_event_source(queue, al_get_display_event_source(disp));
 	al_register_event_source(queue, al_get_timer_event_source(timer));
 	al_register_event_source(queue, al_get_timer_event_source(timerMenu));
 	// BITMAPS
 	ALLEGRO_BITMAP * bitmap = al_load_bitmap("img.jpg");
-	ALLEGRO_BITMAP * bitmap2 = al_load_bitmap("back.png");
+	ALLEGRO_BITMAP * tlo1 = al_load_bitmap("tlo1.png");
+	ALLEGRO_BITMAP * tlo2 = al_load_bitmap("tlo2.png");
 
-	assert(bitmap != NULL);
+	if (!bitmap || !tlo1) { return -1; }
 	// BITMAPS END
 	// INITIAL PART END
 	// RUNTIME VARIABLES
 	ALLEGRO_EVENT event;
+	//ALLEGRO_KEYBOARD_STATE keyState;
+	Config conf;
+	Player player;
+
 	uint16_t choice = 0;
-	float x = 0;
+
 	bool runGame = true;
 	bool menu = true;
 	bool play = false;
+	bool pause = false;
+
 	// RUNTIME VARIABLES END
 	// TIMERS INIT
 	//al_start_timer(timer);
@@ -63,6 +70,7 @@ int main()
 						menu = false;
 						play = true;
 						al_start_timer(timer);
+						al_stop_timer(timerMenu);
 					}
 					else if(choice==1)
 					{
@@ -89,18 +97,20 @@ int main()
 							}
 							else if(event.type == ALLEGRO_EVENT_TIMER && event.timer.source == timerMenu)
 							{
-								al_clear_to_color(al_map_rgb(0, 0, 0));
-								al_draw_text(font2, al_map_rgb(100,50,250), 330, 100, 0, "     Autorzy:     ");
-								al_draw_text(font2, al_map_rgb(100,50,250), 330, 150, 0, "    Dorian Bul    ");
-								al_draw_text(font2, al_map_rgb(100,50,250), 330, 200, 0, "Mariusz Włodarczyk");
-								al_draw_text(font2, al_map_rgb(100,50,250), 330, 250, 0, "     Projekt:     ");
-								al_draw_text(font2, al_map_rgb(100,50,250), 330, 300, 0, "  C++ i Allegro5  ");
+								al_clear_to_color(conf.getRGB(Color::black));
+								al_draw_text(font2, conf.getRGB(Color::purple), conf.menuHorizontal, conf.creditsPos(2), 0, "Autorzy:");
+								al_draw_text(font2, conf.getRGB(Color::orange), conf.menuHorizontal, conf.creditsPos(3), 0, "Dorian Bul");
+								al_draw_text(font2, conf.getRGB(Color::pink), conf.menuHorizontal, conf.creditsPos(4), 0, "Mariusz Włodarczyk");
+								al_draw_text(font2, conf.getRGB(Color::purple), conf.menuHorizontal, conf.creditsPos(5), 0, "Projekt:");
+								al_draw_text(font2, conf.getRGB(Color::purple), conf.menuHorizontal, conf.creditsPos(6), 0, "C++ i Allegro5");
 								al_flip_display();
 							}
 						}
 
 					}
 					else if(choice==4) { runGame = false; menu = false; }
+					break;
+				default:
 					break;
 
 				}
@@ -112,12 +122,12 @@ int main()
 			else if(event.type == ALLEGRO_EVENT_TIMER && event.timer.source == timerMenu)
 			{
 				al_clear_to_color(al_map_rgb(0, 0, 0));
-				al_draw_bitmap(bitmap2, 0, 0, 0);
-				al_draw_text(fontMenu, al_map_rgb(100,50,250), 350, 100, 0, (choice==0)?">  Play  ":"   Play  ");
-				al_draw_text(fontMenu, al_map_rgb(100,50,250), 350, 150, 0, (choice==1)?"> Extras ":"  Extras ");
-				al_draw_text(fontMenu, al_map_rgb(100,50,250), 350, 200, 0, (choice==2)?"> Options":"  Options");
-				al_draw_text(fontMenu, al_map_rgb(100,50,250), 350, 250, 0, (choice==3)?"> Credits":"  Credits");
-				al_draw_text(fontMenu, al_map_rgb(100,50,250), 350, 300, 0, (choice==4)?">  Exit  ":"   Exit  ");
+				al_draw_bitmap(tlo1, 0, 0, 0);
+				al_draw_text(fontMenu, conf.getRGB(Color::orange), conf.menuHorizontal+20, conf.menuPos(1), 0, (choice==0)?">  Play  <":"  Play");
+				al_draw_text(fontMenu, conf.getRGB(Color::orange), conf.menuHorizontal, conf.menuPos(2), 0, (choice==1)?">  Highscores  <":"  Highscores");
+				al_draw_text(fontMenu, conf.getRGB(Color::orange), conf.menuHorizontal+15, conf.menuPos(3), 0, (choice==2)?">  Options  <":"  Options");
+				al_draw_text(fontMenu, conf.getRGB(Color::orange), conf.menuHorizontal+15, conf.menuPos(4), 0, (choice==3)?">  Credits  <":"  Credits");
+				al_draw_text(fontMenu, conf.getRGB(Color::orange), conf.menuHorizontal+20, conf.menuPos(5), 0, (choice==4)?">  Exit  <":"  Exit");
 				al_flip_display();
 			}
 		}
@@ -125,16 +135,58 @@ int main()
 		// GAME LOOP
 		while(play)
 		{
+
 			al_wait_for_event(queue, &event);
 
 			if(event.type == ALLEGRO_EVENT_TIMER)
 			{
-				al_clear_to_color(al_map_rgb(0, 0, 0));
-				al_draw_bitmap(bitmap, x += 0.5, 0, 0);
-				//al_draw_text(font2, al_map_rgb(100,50,250), 150, 200, 0, (choice==0)?">  Play  ":"   Play  ");
-				al_flip_display();
+				if(pause)
+				{
+					al_draw_bitmap(tlo1, 0, 0, 0);
+					al_draw_text(fontMenu, conf.getRGB(Color::purple), 320, conf.menuPos(1), 0, "PAUSE");
+					al_draw_text(fontMenu, conf.getRGB(Color::purple), 120, conf.menuPos(3), 0, "Press any button to continue ");
+					al_draw_text(fontMenu, conf.getRGB(Color::purple), 240, conf.menuPos(4), 0, "Press Esc to Exit");
+					al_flip_display();
+				}
+				else
+				{
+					al_draw_bitmap(tlo2, 0, 0, 0);
+					al_flip_display();
+				}
+
 			}
-			else if((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
+			else if(event.type == ALLEGRO_EVENT_KEY_DOWN)
+			{
+				if(pause)
+				{
+					switch(event.keyboard.keycode)
+					{
+					case ALLEGRO_KEY_ESCAPE:
+						menu = true;
+						play = false;
+						pause = false;
+						al_start_timer(timerMenu);
+						al_stop_timer(timer);
+						break;
+					default:
+						pause = false;
+						break;
+					}
+				}
+				else
+				{
+					switch(event.keyboard.keycode)
+					{
+					case ALLEGRO_KEY_ESCAPE:
+						pause = true;
+						break;
+					default:
+						break;
+					}
+
+				}
+			}
+			else if(event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
 			{
 				runGame = false;
 				break;
@@ -155,7 +207,7 @@ int main()
 	al_destroy_timer(timerMenu);
 	al_destroy_event_queue(queue);
 	al_destroy_bitmap(bitmap);
-	al_destroy_bitmap(bitmap2);
+	al_destroy_bitmap(tlo1);
 	// CLEARING END
 	return 0;
 }
